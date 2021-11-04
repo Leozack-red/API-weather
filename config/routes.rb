126327bc1e09c85291387816770c7c230cb81b7f
Lev <lev.pruglo@mail.ru>
2021-11-04 13:04:06 +0300

    fix: json-response without specifying the parameter

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       root 'current_weather#index'
-      resources :current_weather, only: %i[index], path: 'weather/current'
-      resources :historical_weather, only: %i[index], path: 'weather/historical'
-      resources :max_weather, only: %i[index], path: 'weather/historical/max'
-      resources :min_weather, only: %i[index], path: 'weather/historical/min'
-      resources :average_weather, only: %i[index], path: 'weather/historical/avg'
-      resources :by_time_weather, only: %i[index], path: 'weather/by_time'
-      resources :health_weather, only: %i[index], path: '/health'
+      resources :current_weather, only: %i[index], path: 'weather/current', defaults: { format: 'json' }
+      resources :historical_weather, only: %i[index], path: 'weather/historical', defaults: { format: 'json' }
+      resources :max_weather, only: %i[index], path: 'weather/historical/max', defaults: { format: 'json' }
+      resources :min_weather, only: %i[index], path: 'weather/historical/min', defaults: { format: 'json' }
+      resources :average_weather, only: %i[index], path: 'weather/historical/avg', defaults: { format: 'json' }
+      resources :by_time_weather, only: %i[index], path: 'weather/by_time', defaults: { format: 'json' }
+      resources :health_weather, only: %i[index], path: '/health', defaults: { format: 'json' }
     end
   end
 end
